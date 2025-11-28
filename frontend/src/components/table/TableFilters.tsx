c30555f2cba27f44bdfd9c04b8283950168d7a86
Roman <40374453+romanguerrero@users.noreply.github.com>
2025-11-28 11:46:25 -0800

    style searchbar w/ tailwindcss

--- frontend/src/components/table/TableFilters.tsx
+++ frontend/src/components/table/TableFilters.tsx
@@ -6,7 +6,8 @@ type TableFiltersProps = {
 export const TableFilters = ({ searchValue, setSearchValue }: TableFiltersProps) => {
     return (
         <div>
-            <input type="text" placeholder="Search" value={searchValue} onChange={(e) => setSearchValue(e.target.value)} />
+            <input type="text" placeholder="Search" value={searchValue} onChange={(e) => setSearchValue(e.target.value)}
+            className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" />
         </div>
     )
 }
