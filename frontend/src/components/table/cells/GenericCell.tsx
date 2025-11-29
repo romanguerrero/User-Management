606fbc116837a904644a75ec48ff88a410bd4a47
Roman <40374453+romanguerrero@users.noreply.github.com>
2025-11-29 11:16:11 -0800

    implement generic cell

--- frontend/src/components/table/cells/GenericCell.tsx
+++ frontend/src/components/table/cells/GenericCell.tsx
@@ -1,7 +1,31 @@
-export const GenericCell = () => {
-    return (
-        <div>
-            GenericCell
-        </div>
-    )
+type CellValue = string | number | boolean | Date | null | undefined;
+
+interface GenericCellProps {
+  value?: CellValue;
+}
+
+export function GenericCell({ value }: GenericCellProps) {
+  if (value == null) {
+    return <td>-</td>; 
+  }
+
+  switch (typeof value) {
+    case "string":
+      return <td>{value}</td>;
+
+    case "number":
+      return <td>{value.toString()}</td>;
+
+    case "boolean":
+        return <td>{value ? "True" : "False"}</td>;
+
+    case "object":
+      if (value instanceof Date) {
+        return <td>{value.toDateString()}</td>;
+      }
+      return <td>{JSON.stringify(value)}</td>;
+
+    default:
+      return <td>{String(value)}</td>;
+  }
 }
